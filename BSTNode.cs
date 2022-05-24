71b28f4a94f51ae25c1215ac4220f6effd27e767
Georgi Dinov <georgidinov1@abv.bg>
2022-05-24 12:07:28 +0300

    Added BSTree

---
+++ BSTNode.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+
+    // Binary search tree consists of three nodes called as follows
+    //
+    //        (parent)
+    //         /    \
+    //        /      \
+    //(leftChild) (rightChild)
+    //
+    internal class BSTNode
+    {
+
+        private int data;
+
+        public int Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+
+        private BSTNode leftChild;
+
+        public BSTNode LeftChild
+        {
+            get { return leftChild; }
+            set { leftChild = value; }
+        }
+
+        private BSTNode rightChild;
+
+        public BSTNode RightChild
+        {
+            get { return rightChild; }
+            set { rightChild = value; }
+        }
+
+        public BSTNode(int data)
+        {
+            this.data = data;
+            this.leftChild = null;
+            this.rightChild = null;
+        }
+
+
+        public void Insert(int data)
+        {
+            if (data == this.data)
+            {
+                return; //not to allow duplicates in this implementation
+            }
+
+            if (data < this.data)
+            {
+                if (leftChild == null)
+                {
+                    leftChild = new BSTNode(data);
+                }
+                else
+                {
+                    leftChild.Insert(data);
+                }
+            }
+            else
+            {
+                if (rightChild == null)
+                {
+                    rightChild = new BSTNode(data);
+                }
+                else
+                {
+                    rightChild.Insert(data);
+                }
+            }
+        }
+
+        public BSTNode Get(int data)
+        {
+            if (data == this.data)
+            {
+                return this;
+            }
+
+            if (data < this.data)
+            {
+                if (leftChild != null)
+                {
+                    return leftChild.Get(data);
+                }
+            }
+            else
+            {
+                if (rightChild != null)
+                {
+                    return rightChild.Get(data);
+                }
+            }
+            return null;
+        }
+
+        public int Min()
+        {
+            return leftChild != null ? leftChild.Min() : data;
+        }
+
+        public int Max()
+        {
+            return rightChild != null ? leftChild.Max() : data;
+        }
+
+
+        //traversals with the respect of when e visit the parent node
+
+        public void TraverseInOrder()// visit leftChild, parent and rightChild
+        {
+            if (leftChild != null)
+            {
+                leftChild.TraverseInOrder();
+            }
+
+            Console.Write(this);
+
+            if (rightChild != null)
+            {
+                rightChild.TraverseInOrder();
+            }
+        }
+
+        public void TraversePreOrder()// visit parent, leftChild and rightChild
+        {
+            Console.Write(this);
+
+            if (leftChild != null)
+            {
+                leftChild.TraversePreOrder();
+            }
+
+            if (rightChild != null)
+            {
+                rightChild.TraversePreOrder();
+            }
+        }
+
+        public void TraversePostOrder()// visit leftChild, rightChild and parent
+        {
+            if (leftChild != null)
+            {
+                leftChild.TraversePostOrder();
+            }
+
+            if (rightChild != null)
+            {
+                rightChild.TraversePostOrder();
+            }
+
+            Console.Write(this);
+        }
+
+        public override string ToString()
+        {
+            return "(" + data + ")";
+        }
+
+    }
+}
---
+++ BSTree.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    internal class BSTree
+    {
+
+        private BSTNode root;
+
+        public BSTree()
+        {
+            root = null;
+        }
+
+        public void Insert(int data)
+        {
+            if (root == null)
+            {
+                root = new BSTNode(data);
+            }
+            root.Insert(data);
+        }
+
+        public BSTNode Get(int data)
+        {
+            if (root == null)
+            {
+                throw new Exception("Empty tree!");
+            }
+            return root.Get(data);
+        }
+
+        public void Delete(int data)
+        {
+            root = Delete(root, data);
+        }
+
+        private BSTNode Delete(BSTNode subtreeRoot, int data)
+        {
+            if (subtreeRoot == null)
+            {
+                return subtreeRoot;
+            }
+
+            if (data < subtreeRoot.Data)
+            {
+                subtreeRoot.LeftChild = Delete(subtreeRoot.LeftChild, data);
+            }
+            else if (data > subtreeRoot.Data)
+            {
+                subtreeRoot.RightChild = Delete(subtreeRoot.RightChild, data);
+            }
+            else
+            {
+                // we are at the node which we want to delete
+                // first we handle cases where we have only one or no children
+                // and replace the deleted node with one or the other, or null if there is no children
+                if (subtreeRoot.LeftChild == null)
+                {
+                    return subtreeRoot.RightChild;
+                }
+                if (subtreeRoot.RightChild == null)
+                {
+                    return subtreeRoot.LeftChild;
+                }
+
+                // if we get to here we know that the node we want to delete has two children
+                // we have to pick replacement node for the one we want to delete and preserve the BSTree
+                // we either take the minimum node of the right child, or the maximum node of the left child
+                // set the value of the picked node for replacement to the one we want to delete, and then delete the replacing node from the tree
+                subtreeRoot.Data = subtreeRoot.RightChild.Min();
+                Delete(subtreeRoot.RightChild, subtreeRoot.Data);
+            }
+            return subtreeRoot;
+        }
+
+        public int Min()
+        {
+            if (root == null)
+            {
+                throw new Exception("Empty tree!");
+            }
+            return root.Min();
+        }
+
+        public int Max()
+        {
+            if (root == null)
+            {
+                throw new Exception("Empty tree!");
+            }
+            return root.Max();
+        }
+
+        public void TraverseInOrder()
+        {
+            if (root == null)
+            {
+                throw new Exception("Empty tree!");
+            }
+            root.TraverseInOrder();
+            Console.WriteLine();
+        }
+
+        public void TraversePreOrder()
+        {
+            if (root == null)
+            {
+                throw new Exception("Empty tree!");
+            }
+            root.TraversePreOrder();
+            Console.WriteLine();
+        }
+
+        public void TraversePostOrder()
+        {
+            if (root == null)
+            {
+                throw new Exception("Empty tree!");
+            }
+            root.TraversePostOrder();
+            Console.WriteLine();
+        }
+    }
+}
