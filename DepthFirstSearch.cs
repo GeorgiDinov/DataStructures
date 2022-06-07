7025bc7e610869ba5059740144982ecb787cae5c
Georgi Dinov <georgidinov1@abv.bg>
2022-06-07 23:02:43 +0300

    Added dfs stack and recursion variations

---
+++ DepthFirstSearch.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    internal class DepthFirstSearch
+    {
+
+
+        private Stack<Vertex> stack;
+
+        public DepthFirstSearch()
+        {
+            stack = new Stack<Vertex>();
+        }
+
+
+        public void TraverseDFS(List<Vertex> vertices)
+        {
+            // it may happen to have independent clusters inside our graph
+            foreach (Vertex vertex in vertices)
+            {
+                if (!vertex.IsVisited)
+                {
+                    vertex.IsVisited = true;
+                    DfsHelper(vertex);
+                }
+            }
+        }
+
+        private void DfsHelper(Vertex vertex)
+        {
+            Console.Write("Visiting ");
+            vertex.IsVisited = true;
+            stack.Push(vertex);
+
+            while (stack.Count > 0)
+            {
+                Vertex currentVertex = stack.Pop();
+                Console.Write(currentVertex + " ");
+                foreach (Vertex v in currentVertex.AdjacencyList)
+                {
+                    if (!v.IsVisited)
+                    {
+                        v.IsVisited = true;
+                        stack.Push(v);
+                    }
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ DepthFirstSearchRecursionImpl.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    internal class DepthFirstSearchRecursionImpl
+    {
+
+        public void TraverseDFS(List<Vertex> vertices)
+        {
+            Console.WriteLine("Visiting ");
+            // it may happen to have independent clusters inside our graph
+            foreach (Vertex vertex in vertices)
+            {
+                if (!vertex.IsVisited)
+                {
+                    vertex.IsVisited = true;
+                    DfsHelper(vertex);
+                }
+            }
+            Console.WriteLine();
+        }
+
+        private void DfsHelper(Vertex vertex)
+        {
+            Console.Write(vertex + " ");
+
+            foreach (Vertex v in vertex.AdjacencyList)
+            {
+                if (!v.IsVisited)
+                {
+                    v.IsVisited = true;
+                    DfsHelper(v);
+                }
+            }
+        }
+
+
+    }
+}
+
